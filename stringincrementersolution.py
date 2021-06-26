82263c42fc0eac6e1404e3b8b1433dab8234025c
SleepyGuy2003 <jad-abou-hawili@hotmail.com>
2021-06-26 23:23:59 +0300

    Update stringincrementersolution.py

--- stringincrementersolution.py
+++ stringincrementersolution.py
@@ -1,27 +1,32 @@
+'''increment_string function definition'''
 # make proper modifications to this document stating what it does doc strings, comment it properly, refactor some code, provide clearer strcuture using comments and other methods etc...
-'''
-increment_string(value)
-type(value) is str
 
-What the function does:
-Takes a string and increments its end number (if exists) by adding 1 to it, leading zeros are considered and contribute to the length of the output
-Takes a string and increments it by appending '1' to the end if no number is present.
-'''
 import re
 
 
 def increment_string(strng):
-    # use regular expression to find a match at the end of the string for digit characters
+    '''
+    increment_string(value)
+    type(value) is str
+
+    What the function does:
+    The breif logic of the two algorithms the function chooses from which are based under the first if, else statement respectively:
+    Takes a string and increments its end number (if exists) by adding 1 to it and returns the resultant as string ( leading zeros are considered and contribute to the length of the output
+    Takes a string and increments it by appending '1' to the end if no number is present and returns the resultant string
+    '''
     num_match = re.search(r'\d+$', strng)
     lst = list(strng)
     # keep count of current index in strng
     counter = -1
+    # the first algorithm
+    # check if there is a number at the end of strng
     if num_match:
         digits = num_match.group()
 
-       # check from right to left until hit a number that isn't zero
+        # check from right to left 
         for i in strng:
             counter += 1
+            # until hit first digit of number (number is at end of string)
             if i.isdigit():
                 if ''.join(lst[counter:]) == '9'*len(lst[counter:]):
                     lst[counter:] = str(int(''.join(lst[counter:]))+1)
@@ -44,7 +49,7 @@ def increment_string(strng):
                 else:
 
                     return ''.join(lst)
-
+    # The Second Algorithm
     else:
         return strng+'1'
 
