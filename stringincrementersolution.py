417c3950d05c0c9b60f0dd89ce3c5faf0aa739d6
SleepyGuy2003 <jad-abou-hawili@hotmail.com>
2021-06-30 23:01:32 +0300

    Modifications on comments ( and some spacing )

--- stringincrementersolution.py
+++ stringincrementersolution.py
@@ -1,5 +1,5 @@
 '''increment_string function definition'''
-# make proper modifications to this document stating what it does doc strings, comment it properly, refactor some code, provide clearer strcuture using comments and other methods etc...
+# make proper modifications to this document stating what it does doc strings, comment it properly, provide clearer strcuture using comments and other methods etc...
 
 import re
 
@@ -19,41 +19,49 @@ def increment_string(strng):
     # keep count of current index in strng
     counter = -1
     # the first algorithm
-    # check if there is a number at the end of strng
     if num_match:
         original_num_slice = num_match.group()
+
         # check from right to left
         for i in strng:
+
+            # keep count of current index in strng
             counter += 1
+
             # until hit first digit of number (number is at end of string)
             if i.isdigit():
+
+                # check if end of string is a bunch of nines
                 if ''.join(lst[counter:]) == '9'*len(lst[counter:]):
                     lst[counter:] = str(int(''.join(lst[counter:]))+1)
                     return ''.join(lst)
 
+                # incrementing the number part at the end of the string by 1
                 lst[counter:] = str(int(''.join(lst[counter:]))+1)
                 incremented_num_slice = re.search(
                     r'\d+$', ''.join(lst)).group()
+
+                # checking length of original and incremented num_slice and adjusting the '0' count accordingly
                 if len(incremented_num_slice) < len(original_num_slice):
                     lst[counter:] = (
                         ['0']*(len(original_num_slice)-len(incremented_num_slice)))+lst[counter:]
                     return ''.join(lst)
-
                 if len(incremented_num_slice) > len(original_num_slice):
                     lst[counter:] = list(''.join(lst[counter:]).replace('0', '', len(
                         incremented_num_slice)-len(original_num_slice)))
-                    # here lies the problem
-                    # current logic counter-... in the below is not working
-                    # counter not useful because it gives number and after it, the added 0not detected when replacing, therefore wrong output
                     return ''.join(lst)
 
+                # no need for changes in '0' count
                 else:
 
                     return ''.join(lst)
+
     # The Second Algorithm
+    # There are no digits at the end of strng
     else:
         return strng+'1'
 
 
+# tests to verify algorithm works as described ( in github description )
 print('foobar: '+increment_string('foobar'), 'foobar001 :'+increment_string('foobar001'), 'foobar010: '+increment_string('foobar010'), 'foobar100: '+increment_string('foobar100'),
       'foobar0100: '+increment_string('foobar0100'), 'foobar9: '+increment_string('foobar9'), 'foobar09: '+increment_string('foobar09'), 'foobar009: '+increment_string('foobar009'), sep='\n')
